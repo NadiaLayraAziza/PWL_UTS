c691441e3e71177a2480f38a9741dd9d7ce58c78
NadiaLayraAziza <nadialayra15@gmail.com>
2021-03-30 15:53:16 +0700

    Mengisi kode program pada view detail.blade.php

--- resources/views/tampil/detail.blade.php
+++ resources/views/tampil/detail.blade.php
@@ -1,22 +1,24 @@
-<html>
-    <head>
-        <title>Data Barang</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
-        crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery3.4.1.slim.min.js" 
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" 
-        crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" 
-        crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" 
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" 
-        crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <div class="container">
-        @yield('content')
+@extends('tampil.layout')
+ 
+@section('content')
+
+<div class="container mt-5">
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+        <div class="card-header">
+        Detail Barang
+        </div> <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><b>id_barang : </b>{{$Barang->id_barang}}</li>
+                <li class="list-group-item"><b>kode_barang : </b>{{$Barang->kode_barang}}</li>
+                <li class="list-group-item"><b>nama_barang : </b>{{$Barang->nama_barang}}</li>
+                <li class="list-group-item"><b>kategori_barang : </b>{{$Barang->kategori_barang}}</li>
+                <li class="list-group-item"><b>harga : </b>{{$Barang->harga}}</li>
+                <li class="list-group-item"><b>qty : </b>{{$Barang->qty}}</li>
+            </ul>
         </div>
-    </body>
-</html>
+            <a class="btn btn-success mt-3" href="{{ route('barang.index') }}">Kembali</a>
+        </div>
+    </div>
+</div>
+@endsection
